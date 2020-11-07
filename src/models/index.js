ceaaacc7fae6377002b3837b3568a15552e01e3d
Lissandre <lissandre.pasdeloup@hetic.net>
2020-11-07 19:12:25 +0100

    remove model

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,10 @@
 /**
  * Import your models here
  */
-import personnageSrc from './personnage.glb'
+// import personnageSrc from './personnage.glb'
 
 const modelsList = [
-  { name: 'personnage', src: personnageSrc},
+  // { name: 'personnage', src: personnageSrc},
 ]
 
 /**
