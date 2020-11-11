e091631a48d60582d946b5f72f5a644597e0d968
Lissandre <lissandre.pasdeloup@hetic.net>
2020-11-11 16:27:20 +0100

    sink light

--- src/js/World/Kitchen/Sink.js
+++ src/js/World/Kitchen/Sink.js
@@ -10,16 +10,29 @@ export default class Sink {
     this.container = new THREE.Object3D()
 
     this.createSink()
+    this.addLight()
   }
   createSink() {
     this.sink = this.models.models.sink.scene
     this.sink.traverse((child) => {
       if (child.isMesh) {
+        console.log(child);
         child.material.side = THREE.FrontSide
         child.castShadow = true
         child.receiveShadow = true
+        if(child.material.name === 'Glass') {
+          child.material.emissive = new THREE.Color(0xffffff)
+          child.material.emissiveIntensity = 1
+        }
       }
     })
     this.container.add(this.sink)
+  }
+  addLight() {
+    this.light = new THREE.PointLight(0xffffff, 0.3, 0, 2)
+    this.light.castShadow = true
+    this.light.shadow.camera.near = 0.1
+    this.light.position.set(-2.65, 1.44, 2.2)
+    this.container.add(this.light)
   }
 }
