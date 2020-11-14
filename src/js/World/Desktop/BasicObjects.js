034b3ffcfcb84831926f6f71fef2eb5eb3d480c8
Lissandre <lissandre.pasdeloup@hetic.net>
2020-11-14 17:31:40 +0100

    partial refacto desktop

---
+++ src/js/World/Desktop/BasicObjects.js
@@ -0,0 +1,75 @@
+import * as THREE from 'three'
+import * as CANNON from 'cannon-es'
+
+export default class BasicObjects {
+  constructor(options) {
+    // Options
+    this.time = options.time
+    this.models = options.models.models
+    this.physics = options.physics
+
+    // Set up
+    this.objects = [
+      {src: this.models.computerSupport.scene, mass: 22, container: new THREE.Object3D()},
+      {src: this.models.desktop.scene, mass: 10, container: new THREE.Object3D()},
+      {src: this.models.desktopLeft.scene, mass: 10, container: new THREE.Object3D()},
+      {src: this.models.desktopRight.scene, mass: 10, container: new THREE.Object3D()},
+      {src: this.models.keyboard.scene, mass: 0.1, container: new THREE.Object3D()},
+      {src: this.models.mouse.scene, mass: 0.1, container: new THREE.Object3D()},
+      {src: this.models.mousePad.scene, mass: 0.15, container: new THREE.Object3D()},
+      {src: this.models.phoneSupport.scene, mass: 0.1, container: new THREE.Object3D()},
+      {src: this.models.rubiksCube.scene, mass: 0.1, container: new THREE.Object3D()},
+      {src: this.models.shelfDesktop.scene, mass: 0, container: new THREE.Object3D()},
+      {src: this.models.shelfWall.scene, mass: 0, container: new THREE.Object3D()},
+      {src: this.models.subwooferSpeaker.scene, mass: 3, container: new THREE.Object3D()},
+
+    ]
+
+    this.createObjects()
+    this.setPhysics()
+  }
+  createObjects() {
+    this.objects.forEach((object) => {
+      const obj = object.src
+      obj.traverse((child) => {
+        if (child.isMesh) {
+          child.material.side = THREE.FrontSide
+          child.castShadow = true
+          child.receiveShadow = true
+        }
+      })
+      object.container.add(obj)
+    })
+  }
+  setPhysics() {
+    this.objects.forEach((object) => {
+      const size = new THREE.Vector3()
+      const center = new THREE.Vector3()
+      const calcBox = new THREE.Box3().setFromObject( object.container )
+
+      calcBox.getSize(size)
+      size.x *= 0.5
+      size.y *= 0.5
+      size.z *= 0.5
+      calcBox.getCenter(center)
+
+      const box = new CANNON.Box(new CANNON.Vec3().copy(size))
+      object.container.body = new CANNON.Body({
+        mass: object.mass,
+        position: center
+      })
+
+      object.container.body.addShape(box)
+      this.physics.world.addBody(object.container.body)
+
+      this.time.on('tick', () => {
+        object.container.quaternion.copy(object.container.body.quaternion)
+        object.container.position.set(
+          object.container.body.position.x - center.x,
+          object.container.body.position.y - center.y,
+          object.container.body.position.z - center.z,
+          )
+      })
+    })
+  }
+}
--- src/js/World/Desktop/index.js
+++ src/js/World/Desktop/index.js
@@ -1,27 +1,15 @@
 import * as THREE from 'three'
 
 import Computer from './Computer.js'
-import ComputerSupport from './ComputerSupport.js'
-import Desktop from './Desktop.js'
-import DesktopLeft from './DesktopLeft.js'
-import DesktopRight from './DesktopRight.js'
+import BasicObjects from './BasicObjects.js'
 import Headset from './Headset.js'
-import Keyboard from './Keyboard.js'
-import Mouse from './Mouse.js'
-import MousePad from './MousePad.js'
 import Music from './SetMusic.js'
-import Phone from './Phone.js'
-import PhoneSupport from './PhoneSupport.js'
-import RubiksCube from './RubiksCube.js'
-import Seat from './Seat.js'
+// import Phone from './Phone.js'
 import ScreenLeft from './ScreenLeft.js'
 import ScreenRight from './ScreenRight.js'
 import ScreenSupport from './ScreenSupport.js'
-import ShelfDesktop from './ShelfDesktop.js'
-import ShelfWall from './ShelfWall.js'
 import SpeakerLeft from './SpeakerLeft.js'
 import SpeakerRight from './SpeakerRight.js'
-import SubwooferSpeaker from './SubwooferSpeaker.js'
 
 export default class DesktopPlace {
   constructor(options) {
@@ -35,27 +23,15 @@ export default class DesktopPlace {
     this.container = new THREE.Object3D()
 
     this.setComputer()
-    this.setComputerSupport()
-    this.setDesktop()
-    this.setDesktopLeft()
-    this.setDesktopRight()
+    this.setBasicObjects()
     this.setHeadset()
-    this.setKeyboard()
-    this.setMouse()
-    this.setMousePad()
     // this.setPhone()
-    this.setPhoneSupport()
-    this.setRubiksCube()
-    // this.setSeat()
     this.setScreenLeft()
     this.setScreenRight()
     this.setScreenSupport()
-    this.setShelfDesktop()
-    this.setShelfWall()
     this.setSpeakerLeft()
     this.setSpeakerRight()
     this.setMusic()
-    this.setSubwooferSpeaker()
   }
   setComputer() {
     this.computer = new Computer({
@@ -65,37 +41,15 @@ export default class DesktopPlace {
     })
     this.container.add(this.computer.container)
   }
-  setComputerSupport() {
-    this.computerSupport = new ComputerSupport({
+  setBasicObjects() {
+    this.basicObjects = new BasicObjects({
       models: this.models,
       time: this.time,
       physics: this.physics,
     })
-    this.container.add(this.computerSupport.container)
-  }
-  setDesktop() {
-    this.desktop = new Desktop({
-      models: this.models,
-      time: this.time,
-      physics: this.physics,
+    this.basicObjects.objects.forEach(object => {
+      this.container.add(object.container)
     })
-    this.container.add(this.desktop.container)
-  }
-  setDesktopLeft() {
-    this.desktopLeft = new DesktopLeft({
-      models: this.models,
-      time: this.time,
-      physics: this.physics,
-    })
-    this.container.add(this.desktopLeft.container)
-  }
-  setDesktopRight() {
-    this.desktopRight = new DesktopRight({
-      models: this.models,
-      time: this.time,
-      physics: this.physics,
-    })
-    this.container.add(this.desktopRight.container)
   }
   setHeadset() {
     this.headset = new Headset({
@@ -105,30 +59,6 @@ export default class DesktopPlace {
     })
     this.container.add(this.headset.container)
   }
-  setKeyboard() {
-    this.keyboard = new Keyboard({
-      models: this.models,
-      time: this.time,
-      physics: this.physics,
-    })
-    this.container.add(this.keyboard.container)
-  }
-  setMouse() {
-    this.mouse = new Mouse({
-      models: this.models,
-      time: this.time,
-      physics: this.physics,
-    })
-    this.container.add(this.mouse.container)
-  }
-  setMousePad() {
-    this.mousePad = new MousePad({
-      models: this.models,
-      time: this.time,
-      physics: this.physics,
-    })
-    this.container.add(this.mousePad.container)
-  }
   setPhone() {
     this.phone = new Phone({
       models: this.models,
@@ -136,30 +66,6 @@ export default class DesktopPlace {
       physics: this.physics,
     })
     this.container.add(this.phone.container)
-  }
-  setPhoneSupport() {
-    this.phoneSupport = new PhoneSupport({
-      models: this.models,
-      time: this.time,
-      physics: this.physics,
-    })
-    this.container.add(this.phoneSupport.container)
-  }
-  setRubiksCube() {
-    this.rubiksCube = new RubiksCube({
-      models: this.models,
-      time: this.time,
-      physics: this.physics,
-    })
-    this.container.add(this.rubiksCube.container)
-  }
-  setSeat() {
-    this.seat = new Seat({
-      models: this.models,
-      time: this.time,
-      physics: this.physics,
-    })
-    this.container.add(this.seat.container)
   }
   setScreenLeft() {
     this.screenLeft = new ScreenLeft({
@@ -185,22 +91,6 @@ export default class DesktopPlace {
     })
     this.container.add(this.screenSupport.container)
   }
-  setShelfDesktop() {
-    this.shelfDesktop = new ShelfDesktop({
-      models: this.models,
-      time: this.time,
-      physics: this.physics,
-    })
-    this.container.add(this.shelfDesktop.container)
-  }
-  setShelfWall() {
-    this.shelfWall = new ShelfWall({
-      models: this.models,
-      time: this.time,
-      physics: this.physics,
-    })
-    this.container.add(this.shelfWall.container)
-  }
   setSpeakerLeft() {
     this.speakerLeft = new SpeakerLeft({
       models: this.models,
@@ -224,13 +114,5 @@ export default class DesktopPlace {
       speakerRight: this.speakerRight,
       speakerLeft: this.speakerLeft
     })
-  }
-  setSubwooferSpeaker() {
-    this.subwooferSpeaker = new SubwooferSpeaker({
-      models: this.models,
-      time: this.time,
-      physics: this.physics,
-    })
-    this.container.add(this.subwooferSpeaker.container)
   }
 }
